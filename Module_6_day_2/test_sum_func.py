810ddfd14a64e001a1e9adfe6a94aa108861fca5
talha6003 <muhammadtalha856@gmail.com>
2023-05-24 09:50:35 +0500

    adding more functions.

--- Module_6_day_2/test_sum_func.py
+++ Module_6_day_2/test_sum_func.py
@@ -1,4 +1,5 @@
 from sum_func import add, add_positive
+import pytest
 # Tests
 def test_add_positive_2_and_3():   
     assert add_positive(2, 3) == 5
@@ -6,7 +7,7 @@ def test_add_positive_2_and_3():
 def test_add_negative_5_and_positive_10():
     assert add_positive(-5, 10) == None
 
-def test_type_check():
+def test_add_positive_type_check():
     assert type(add_positive(2,3))==int 
 
 def test_add_positive_10_and_negative_5():
@@ -20,3 +21,35 @@ def test_add_0_and_0():
 
 def test_add_2_and_3():
     assert add(2,3) == 5
+
+def test_add_positive_integer_and_string():
+    with pytest.raises(TypeError):
+        add_positive(2, "3")
+
+def test_add_positive_string_and_integer():
+    with pytest.raises(TypeError):
+        add_positive("2", 3)
+
+def test_add_string_and_integer():
+    with pytest.raises(TypeError):
+        add("2", 3)
+
+def test_add_integer_and_string():
+    with pytest.raises(TypeError):
+        add(2, "3")
+
+def test_add_positive_integer_and_float():
+    with pytest.raises(TypeError):
+        add_positive(2.5, "3")
+
+def test_add_positive_float_and_integer():
+    with pytest.raises(TypeError):
+        add_positive("2", 3.5)
+
+def test_add_float_and_integer():
+    with pytest.raises(TypeError):
+        add("2", 3.5)
+
+def test_add_integer_and_float():
+    with pytest.raises(TypeError):
+        add(2.5, "3")
