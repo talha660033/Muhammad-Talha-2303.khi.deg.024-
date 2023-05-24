ea2e77d5927e1a3808edbca5870647cbb339d20a
talha6003 <muhammadtalha856@gmail.com>
2023-05-24 08:49:07 +0500

    Updated Files and ScreenShots.

--- Module_6_day_2/test_sum_func.py
+++ Module_6_day_2/test_sum_func.py
@@ -1,7 +1,16 @@
 from sum_func import add_positive
 # Tests
-def test_add_positive():
+def test_add_positive_2_and_3():   
     assert add_positive(2, 3) == 5
+
+def test_add_negative_5_and_positive_10():
     assert add_positive(-5, 10) == None
-    assert add_positive(0, 0) == None
+
+def test_type_check():
     assert type(add_positive(2,3))==int 
+
+def test_add_positive_10_and_negative_5():
+    assert add_positive(10, -5) == None
+
+def test_add_positive_0_and_0():
+    assert add_positive(0, 0) == None
